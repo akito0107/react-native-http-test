68eeca0e5d70f44994a0e722881880b71af498db
Akito Ito <akito0107@gmail.com>
2016-12-11 04:00:46 +0900

    update ws to bin server

--- server/ws-server.js
+++ server/ws-server.js
@@ -2,12 +2,44 @@
 
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({port: 3000})
+const Transform = require('stream').Transform;
+const Writable = require('stream').Writable;
+const net = require('net');
+
+const stream = new Transform({
+  transform: function(chunk, enc, cb) {
+    let data;
+    try {
+      data = JSON.parse(chunk);
+    } catch (e){
+      return cb(null);
+    }
+    let {x, y, z} = data.message.acceleration;
+    this.push(JSON.stringify({x, y, z}));
+    cb(null)
+  }
+});
 
 wss.on('connection', (ws) => {
   console.log('on connection')
-
   ws.on('message', (mes) => {
-    console.log(mes)
-    // ws.send(JSON.stringify({ok: 'ack'}))
-  })
-})
+    stream.write(mes);
+  });
+});
+
+const server = net.createServer((socket) => {
+  stream.pipe(socket);
+  socket.on('data', (data) => {
+    console.log(`Bin Server Got Data: ${data}`);
+  });
+
+}).listen({port: 12345}, () => {
+  console.log(`Bin Server Started`)
+});
+
+stream.pipe(new Writable({
+  write: function(chunk, enc, cb) {
+    console.log(`Writable Got: ${chunk}`)
+    cb(null);
+  }
+}));
